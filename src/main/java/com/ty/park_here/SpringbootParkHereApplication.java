0b64b3ad41195ae3bc39f52e6d1ff24c7b45770f
SuriyaprakashSr <suriyaprakashsr86@gmail.com>
2022-12-06 12:46:16 +0530

    Spring boot parkhere project

---
+++ src/main/java/com/ty/park_here/SpringbootParkHereApplication.java
@@ -0,0 +1,13 @@
+package com.ty.park_here;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootParkHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootParkHereApplication.class, args);
+	}
+
+}
