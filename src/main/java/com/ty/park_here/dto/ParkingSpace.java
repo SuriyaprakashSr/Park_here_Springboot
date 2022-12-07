a756e4b33e7e07fcdd9c7a7e956d388ca718bcfb
SuriyaprakashSr <suriyaprakashsr86@gmail.com>
2022-12-07 12:10:19 +0530

    Parkingspace Dto added

--- src/main/java/com/ty/park_here/dto/ParkingSpace.java
+++ src/main/java/com/ty/park_here/dto/ParkingSpace.java
@@ -1,5 +1,33 @@
 package com.ty.park_here.dto;
 
-public class ParkingSpace {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class ParkingSpace {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	private String parkingSpaceName;
+	@NotNull
+	private int availableSpace;
+	@NotNull
+	private int utilizedSpace;
+	@NotNull
+	private int totalSpace;
+	@NotNull
+	private int rentPerHour;
+	
+	
 }
