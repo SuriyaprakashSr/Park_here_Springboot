1f4b68aeca6cb09c9683d86033b8f345aba12280
abhishekjavadevloper <abhishekjavadeveloper.com>
2022-12-07 13:03:40 +0530

    User Repository

--- src/main/java/com/ty/park_here/repository/UserRepository.java
+++ src/main/java/com/ty/park_here/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.ty.park_here.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.park_here.dto.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
 
 }
