519f640a7717aed3e6efe5f421c223136fa39494
SuriyaprakashSr <suriyaprakashsr86@gmail.com>
2022-12-08 11:38:51 +0530

    Exception Handler Class upated

--- src/main/java/com/ty/park_here/exceptionhandler/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/park_here/exceptionhandler/ApplicationExceptionHandler.java
@@ -1,5 +1,45 @@
 package com.ty.park_here.exceptionhandler;
 
-public class ApplicationExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.ty.park_here.util.ResponseStructure;
+import com.ty.park_here.exception.NoSuchIdFoundException;
+import com.ty.park_here.exception.NoSuchNameFoundException;
+import com.ty.park_here.exception.UnableToUpdateException;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(NoSuchIdFoundException.class)
+	public ResponseEntity<ResponseStructure<?>> NoSuchIdFoundException(NoSuchIdFoundException exception) {
+		ResponseStructure<String> responseStructure = new ResponseStructure<>();
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("No such id found");
+		responseStructure.setData(exception.getMessage());
+		return new ResponseEntity<ResponseStructure<?>>(responseStructure,HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@ExceptionHandler(NoSuchNameFoundException.class)
+	public ResponseEntity<ResponseStructure<?>> NoSuchNameFoundException(NoSuchNameFoundException exception){
+		ResponseStructure<String> responseStructure = new ResponseStructure<>();
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("No Such Name Found");
+		responseStructure.setData(exception.getMessage());
+		return new ResponseEntity<ResponseStructure<?>>(responseStructure,HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(UnableToUpdateException.class)
+	public ResponseEntity<ResponseStructure<?>> UnableToUpdateException(UnableToUpdateException exception){
+		ResponseStructure<String> responseStructure = new ResponseStructure<>();
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("Unable to update ");
+		responseStructure.setData(exception.getMessage());
+		return new ResponseEntity<ResponseStructure<?>>(responseStructure,HttpStatus.NOT_FOUND); 
+	}
 
 }
