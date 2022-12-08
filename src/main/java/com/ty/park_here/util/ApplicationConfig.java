f9ac7ad645444a3de57734be3deb7d23665ed85c
abhishekjavadevloper <abhishekjavadeveloper.com>
2022-12-08 16:53:06 +0530

    Application config done

---
+++ src/main/java/com/ty/park_here/util/ApplicationConfig.java
@@ -0,0 +1,28 @@
+package com.ty.park_here.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class ApplicationConfig {
+
+	public Docket getDocket() {
+		Contact contact = new Contact("ty", "testYantra.com", "tastyantra@gmail.com");
+		List<VendorExtension> extensions = new ArrayList<VendorExtension>();
+		ApiInfo apiInfo = new ApiInfo("Park Here", "Boot app for Parking management", "1.0", "testyantra.com", contact,
+				"apache tomcat", "www.apache.com", extensions);
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.ty.park_here")).build();
+	}
+
+}
