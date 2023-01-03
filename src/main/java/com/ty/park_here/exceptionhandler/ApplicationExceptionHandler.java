3883ed373748de18db72cbcc30a6db74ca6144cf
SuriyaprakashSr <suriyaprakashsr86@gmail.com>
2023-01-03 11:45:00 +0530

    Validation field exception class added

--- src/main/java/com/ty/park_here/exceptionhandler/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/park_here/exceptionhandler/ApplicationExceptionHandler.java
@@ -1,9 +1,20 @@
 package com.ty.park_here.exceptionhandler;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ty.park_here.util.ResponseStructure;
@@ -14,8 +25,30 @@ import com.ty.park_here.exception.UnableToDeleteLocationException;
 import com.ty.park_here.exception.UnableToUpdateException;
 import com.ty.park_here.exception.UnableToUpdateLocation;
 
+
+
 @RestControllerAdvice
 public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<ObjectError> errors = ex.getAllErrors();
+		Map<String, String> map = new LinkedHashMap<>();	
+		for(ObjectError  error:errors) {
+			String message =error.getDefaultMessage();
+			String field =((FieldError)error).getField();
+			map.put(field, message);
+			
+		}
+		ResponseStructure<Map<String,String>> responseStructure = new ResponseStructure<>();
+		responseStructure.setStatus(HttpStatus.BAD_REQUEST.value());
+		responseStructure.setMessage(HttpStatus.BAD_REQUEST.name());
+		responseStructure.setData(map);
+		
+		return  new ResponseEntity<Object>(responseStructure,HttpStatus.BAD_REQUEST) ;
+	
+		
+	}
 
 	@ExceptionHandler(NoSuchIdFoundException.class)
 	public ResponseEntity<ResponseStructure<?>> noSuchIdFoundException(NoSuchIdFoundException exception) {
@@ -72,4 +105,16 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 		responseStructure.setData(exception.getMessage());
 		return new ResponseEntity<ResponseStructure<?>>(responseStructure, HttpStatus.NOT_FOUND);
 	}
+
+//	@ExceptionHandler(ConstraintViolationException.class)
+//	public ResponseEntity<ResponseStructure<?>> handleConstraintViolationException(ConstraintViolationException exception){
+//		ResponseStructure<String> responseStructure = new ResponseStructure<String>();
+//		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+//		responseStructure.setMessage("Enter proper fields ");
+//		responseStructure.setData(exception.getMessage());
+//		return new ResponseEntity<ResponseStructure<?>>(responseStructure, HttpStatus.NOT_FOUND);
+//	}
+
+	
+	
 }
